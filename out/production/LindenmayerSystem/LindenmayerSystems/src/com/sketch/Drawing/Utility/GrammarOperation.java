4a6aee20cbf4da8500dd86dcd539420bbc32c4fd
Benjamin Bruland <benbruland@gmail.com>
2024-08-08 16:54:53 -0500

    Ignore out directory in .gitignore

--- out/production/LindenmayerSystem/LindenmayerSystems/src/com/sketch/Drawing/Utility/GrammarOperation.java
+++
@@ -1,7 +0,0 @@
-package com.sketch.Drawing.Utility;
-
-public class GrammarOperation {
-    public String operationName;
-    public String operationSymbol;
-    public WeightedItem<String>[] operationExpansionWeights;
-}
